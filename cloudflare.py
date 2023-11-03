5161366f474311fa3537a0144aa0ad463ee9868c
Jetsung Chan <jetsungchan@gmail.com>
2023-11-03 18:51:25 +0800

    refactor: code decoupling for gcore,cloudflare

--- cloudflare.py
+++ cloudflare.py
@@ -3,8 +3,10 @@ import subprocess
 import ipaddress
 import os
 import requests
+from check import Check
 
 from dns_cf import main as cf_refresh
+
 
 def ip_from_fetch():
     '''
@@ -19,6 +21,7 @@ def ip_from_fetch():
     ip_list = ip_string.strip().splitlines()
     return [ip.strip() for ip in ip_list if ip.strip()]
 
+
 def ip_from_file():
     '''
     从 ip.txt 文件中读取 IP 段
@@ -31,6 +34,7 @@ def ip_from_file():
 
     with open('ip.txt', 'r') as file:
         return file.read().splitlines()
+
 
 def ip_from_string():
     '''
@@ -67,63 +71,74 @@ def ip_from_string():
     ip_list = ip_string.strip().splitlines()
     return [ip.strip() for ip in ip_list if ip.strip()]
 
+
 def run():
     try:
-        # 存储所有测速结果的字典
-        speed_results = []
+        # 从环境变量中读取 CLOUDFLARE_DOMAIN, CLOUDFLARE_TOKEN
+        domain = os.environ.get('CLOUDFLARE_DOMAIN')
+        token = os.environ.get('CLOUDFLARE_TOKEN')
+
+        skip = os.environ.get('CLOUDFLARE_VALID_SKIP')
+
+        check = Check()
+
+        # 源IP有效开关
+        if skip:
+            old_ip = check.domain_ip(domain)
+            # 源IP有效则返回
+            if old_ip:
+                return old_ip
+
+        # 从环境变量中读取 CLOUDFLARE_RANDOM_NUM
+        cloudflare_random_num_str = os.environ.get('CLOUDFLARE_RANDOM_NUM', '0')
+        try:
+            cloudflare_random_num = int(cloudflare_random_num_str)
+        except ValueError:
+            # 处理无法转换为整数的情况
+            # print("CLOUDFLARE_RANDOM_NUM 不是有效的整数，将使用默认值 0。")
+            cloudflare_random_num = 3  # 默认值或其他处理方式
+
         ip_ranges = ip_from_fetch()
         # ip_ranges = ip_from_file()
         # ip_ranges = ip_from_string()
 
+        ip_list = []
         # 遍历每个 IP 段
         for ip_range in ip_ranges:
             # 解析 IP 段
             ip_network = ipaddress.IPv4Network(ip_range, strict=False)
-            
+
             # 随机选择 3 个 IP 地址
-            num_ips = min(3, ip_network.num_addresses - 2)  # 减去网络地址和广播地址
+            num_ips = min(cloudflare_random_num, ip_network.num_addresses - 2)  # 减去网络地址和广播地址
             ips = [str(ip) for ip in random.sample(list(ip_network.hosts()), num_ips)]
-            
-            # 测速并记录结果
-            speeds = []
-            for ip in ips:
-                try:
-                    result = subprocess.check_output(['ping', '-c', '3', '-W', '5', ip])
-                    lines = result.decode('utf-8').splitlines()
-                    for line in lines:
-                        if 'avg' in line:
-                            speed = float(line.split('/')[4])
-                            speeds.append((ip, speed))
-                except subprocess.CalledProcessError:
-                    pass
-            
-            # 如果至少有一个 IP 测速成功，则记录最快的 IP 地址和速度
-            if speeds:
-                fastest_ip, fastest_speed = min(speeds, key=lambda x: x[1])
-                speed_results.append((fastest_ip, fastest_speed))
+            ip_list.extend(ips)
+        print(f'ip count: {len(ip_list)}')
 
-        print(f'valid ip count: {len(speed_results)}')
+        valid_ips = check.run(ip_list)
+        print(f'valid ip count: {len(valid_ips)}')
 
-        if len(speed_results) == 0:
+        if len(valid_ips) == 0:
             raise ValueError(f'No valid IP address')
 
-        sorted_results = sorted(speed_results, key=lambda x: x[1])
-        print(f'Fast CloudFlare IP: {sorted_results[0]}')
+        # 按响应时间从小到大排序
+        valid_ips.sort(key=lambda x: x[1])
+        print(f'Fast CloudFlare IP: {valid_ips[0]}')
 
-        best_ip = sorted_results[0][0]
-        refresh(best_ip)
+        best_ip = valid_ips[0][0]
+
+        # 更新 CF IP
+        refresh(best_ip, domain, token)
+
         # 若更新 cloudflare ns 成功，则返回该 IP
         return best_ip
 
     except Exception as e:
         print(f'Failed to get cloudflare best ip.  {e}')
 
-def refresh(ip):
+
+def refresh(ip, domain, token):
     '''
     更新 cloudflare 的 IP 到 cloudflare
     '''
-    # 从环境变量中读取 CLOUDFLARE_DOMAIN, CLOUDFLARE_TOKEN
-    domain = os.environ.get('CLOUDFLARE_DOMAIN')
-    token = os.environ.get('CLOUDFLARE_TOKEN')
     if domain and token:
         cf_refresh(ip_address=ip, record_name=domain, token=token)
